44001bf397c1894982649aa32c6109a30532cea4
Blayne <ZeroG001@hotmail.com>
2014-03-13 04:00:05 -0400

    partially added ability to dynamically expand text box when entering text. Trying to follow as closely to google keep as I can. Mainly changes were made in test.js

--- js/test.js
+++ js/test.js
@@ -1,10 +1,29 @@
-$(document).ready(function(){
-	$('.problem').keyup(function (event) {
-		console.log(event.which);
+$(document).ready(function () {
 
-		if (event.which == 13) {
-			$('body').append("<p> hello </p>");
-		};
+
+	$('.problem-detail').keyup(function (event){
+		var apples = $('.problem-detail').val().split(/(\n|\r)/);
+
+		console.log($('.problem-detail').scrollTop());
+		var topvalue = $('.problem-detail').scrollTop();
+		var heightvalue = $(".problem-detail").height();
+	
+			$(".problem-detail").height(heightvalue+topvalue+"px");
+
+		// if (event.which == 13) {
+		// 	$(".problem-detail").height(apples.length*19+"px");
+		// }
+
+		// if (event.which == 8) {
+		// 	$(".problem-detail").height(apples.length*19+"px");
+		}
+	});
+
+	$('.problem-submit').click(function() {
+
+			
+			$('.row').append("<div class='col-xs-6 col-sm-6 col-md-4 col-lg-4' style='border: 1px solid black'>" + $('.problem-detail').val() + "</div>");
+		
 	});
 
 });
