0a2d9daad35b466c48a6f16be3bbd2f5bcd7365d
Blayne <ZeroG001@hotmail.com>
2014-03-13 04:05:35 -0400

    fixed (very) small bug in test.js

--- js/test.js
+++ js/test.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
 
 
 	$('.problem-detail').keyup(function (event){
-		var apples = $('.problem-detail').val().split(/(\n|\r)/);
+		// var apples = $('.problem-detail').val().split(/(\n|\r)/);
 
 		console.log($('.problem-detail').scrollTop());
 		var topvalue = $('.problem-detail').scrollTop();
 		var heightvalue = $(".problem-detail").height();
 	
 			$(".problem-detail").height(heightvalue+topvalue+"px");
+			console.log("teok");
 
 		// if (event.which == 13) {
 		// 	$(".problem-detail").height(apples.length*19+"px");
@@ -16,7 +17,7 @@ $(document).ready(function () {
 
 		// if (event.which == 8) {
 		// 	$(".problem-detail").height(apples.length*19+"px");
-		}
+		// }
 	});
 
 	$('.problem-submit').click(function() {
